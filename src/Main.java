d7113806f68657bc8335342c341be89e0878109a
Andrey <kovorot0875@mail.ru>
2023-03-22 22:40:40 +0300

    main method built

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static final String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " +
@@ -6,6 +9,30 @@ public class Main {
             "esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa " +
             "qui officia deserunt mollit anim id est laborum.";
     public static void main(String[] args) {
+        Map<Character, Integer> symbols = new HashMap<>();
+        int length = text.length();
+        int number = 0;
+        while(number < length) {
+            if (symbols.containsKey(text.charAt(number))) {
+                symbols.put(text.charAt(number), symbols.get(text.charAt(number)) + 1);
+            } else {
+                symbols.put(text.charAt(number), 1);
+            }
+            number++;
+        }
 
+        int maxValue = 0;
+        int minValue = text.length();
+
+        for (Map.Entry<Character, Integer> kv : symbols.entrySet()) {
+            if (kv.getValue() > maxValue) {
+                maxValue = kv.getValue();
+            } else if (kv.getValue() < minValue) {
+                minValue = kv.getValue();
+            }
+        }
+
+        System.out.println("Максимальное значение: " + maxValue);
+        System.out.println("Минимальное значение: " + minValue);
     }
 }
